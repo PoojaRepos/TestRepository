cfc01b8c42a667ece141dd713a51169e9cbcc6d1
DELL <DELL@DESKTOP-B71EELU>
2023-05-27 16:18:50 +0530

    factorial changes

--- SeleniumProject/src/program/factorial.java
+++ SeleniumProject/src/program/factorial.java
@@ -3,8 +3,13 @@ package program;
 public class factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		int num = 8,fact =1;
+		for (int i=1;i<=num;i++) {
+			fact = fact*i;
+		}
+		System.out.println("The results of the factorial number is " + fact);
 	}
 
 }
